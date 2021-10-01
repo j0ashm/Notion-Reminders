c04701d1d20d4bf6ee7c7918b032d67f778fd81e
JustinOleskii <joashm14@gmail.com>
2021-10-01 09:07:48 +0530

    write functions to get data from db and check their status

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const { Client } = require("@notionhq/client")
+const dayjs = require('dayjs')
+
+var relativeTime = require('dayjs/plugin/relativeTime')
+dayjs.extend(relativeTime)
+
 require('dotenv').config()
 
 const notion = new Client({ 
@@ -7,7 +12,37 @@ const notion = new Client({
 
 const databaseId = process.env.NOTION_DB
 
-async function getTasks() {
+const taskObject = {} 
+
+async function populateDateStore() {
+    const currentTasks = await getTasksFromDb()
+
+    for (const { pageId, dueDate, title } of currentTasks) {
+        taskObject[title] = {dueDate, pageId}
+    }
+}
+
+async function checkForCompletion(tasks) {
+    for (const task in tasks) {
+        const pageStats = await notion.pages.retrieve({page_id: tasks[task].pageId})
+
+        if (pageStats.properties['Complete'].checkbox === true) {
+            console.log('Archiving page...')
+            notion.pages.update({
+                page_id: tasks[task].pageId,
+                archived: true
+            })
+            console.log(`Removing ${tasks[task]} from the active task list...`)
+            delete taskObject[task]
+            console.log(taskObject)
+            console.log('Deletion complete.')
+        }
+    }
+
+    return tasks
+}
+
+async function getTasksFromDb() {
     const pages = []
     let cursor = undefined
 
@@ -22,12 +57,13 @@ async function getTasks() {
     }
     console.log(`${pages.length} items were successfully fetched from database.`)
 
-    pages.map(page => {
+    return pages.map(page => {
         const dueDateProperty = page.properties['Due'].date['start']
         const titleProp = page.properties['Name'].title[0]['plain_text']
-
-        console.log(dueDateProperty, titleProp)
+        return {
+            pageId: page.id,
+            dueDate: dueDateProperty,
+            title: titleProp
+        }
     })
 }
-
-getTasks()
